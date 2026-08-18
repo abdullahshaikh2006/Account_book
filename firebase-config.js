f1d442e3ca31da354985f192397e1aad54d7483e
abdullahshaikh2006 <md.abdullahsheikh.fresh@gmail.com>
2026-08-18 23:11:56 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,16 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB16ZrdZ2IY_R8aSZipz-O6SelEblngJLE",
+    authDomain: "marketing-calculation.firebaseapp.com",
+    databaseURL: "https://marketing-calculation-default-rtdb.firebaseio.com",
+    projectId: "marketing-calculation",
+    storageBucket: "marketing-calculation.firebasestorage.app",
+    messagingSenderId: "371475369216",
+    appId: "1:371475369216:web:3aff3d68b707f65c1e77e5"
+};
 
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+const auth = firebase.auth();
+const database = firebase.database();
